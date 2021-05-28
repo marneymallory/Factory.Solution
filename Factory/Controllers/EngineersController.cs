fddc2fc9d4c281897c8ea5bdcadf9fdbad49b933
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-28 09:31:08 -0800

    WIP-deleted project twice...just got everything renamed

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+
+namespace FactoryControllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Engineers.ToList());
+    }
+  }
+}
